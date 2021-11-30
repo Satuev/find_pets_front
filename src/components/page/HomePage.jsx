b966784db40b82cecf01728a0e13942a5289e147
Зелимхан Демильханов <zelemhanzzz@mail.ru>
2021-11-30 14:20:41 +0300

    заполнил начальный стейт, исправил роуты

--- src/components/page/HomePage.jsx
+++ src/components/page/HomePage.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch} from "react-redux";
+import {fetchCategories, fetchPets, fetchUsers} from "../../redux/features/data_base";
 
 const HomePage = () => {
+
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+        dispatch(fetchCategories());
+    })
+    useEffect(()=>{
+        dispatch(fetchUsers());
+    })
+    useEffect(()=>{
+        dispatch(fetchPets());
+    })
+
     return (
         <div>
             //здесь должен быть хеадер
--- src/redux/features/data_base.js
+++ src/redux/features/data_base.js
@@ -1,12 +1,66 @@
 const initialState = {
   users: [],
   categories: [],
-  pats:[]
+  pets:[],
+  pending: false
 };
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case "load/pending":
+      return {
+        ...state, pending: true
+      }
+
+      case "categories/load/fulfilled":
+      return {
+        ...state, categories: action.payload, pending: false
+      }
+      case "users/load/fulfilled":
+      return {
+        ...state, users: action.payload, pending: false
+      }
+      case "pets/load/fulfilled":
+      return {
+        ...state, pets: action.payload, pending: false
+      }
+
     default:
       return state;
   }
+}
+
+export const fetchCategories = () => {
+  return (dispatch) => {
+    dispatch({type: "load/pending"})
+    fetch("http://localhost:6557/categories/")
+        .then((res) => res.json())
+        .then((data) => {
+          dispatch({ type: "categories/load/fulfilled", payload: data });
+        });
+  };
 };
+export const fetchUsers = () => {
+  return (dispatch) => {
+    dispatch({type: "load/pending"})
+    fetch("http://localhost:6557/users")
+        .then((res) => res.json())
+        .then((data) => {
+          dispatch({ type: "users/load/fulfilled", payload: data });
+        });
+  };
+};
+export const fetchPets = () => {
+  return (dispatch) => {
+    dispatch({type: "load/pending"})
+    fetch("http://localhost:6557/pets")
+        .then((res) => res.json())
+        .then((data) => {
+          dispatch({ type: "pets/load/fulfilled", payload: data });
+        });
+  };
+};
+
+
+
+
