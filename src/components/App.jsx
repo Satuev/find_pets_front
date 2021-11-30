ce7788410daccf85975506cccbcd5223d071d884
Satuev <alitm.s@mail.ru>
2021-12-01 00:22:13 +0300

    добавил вывод питомцев, карусель, лого

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-import {Route, Routes} from "react-router-dom";
-import HomePage from "./page/HomePage";
-import SignIn from "./page/SignIn";
-import SignUp from "./page/SignUp";
+import { Route, Routes } from 'react-router-dom'
+import Header from './Header'
+import HomePage from './page/HomePage'
+import Pets from './page/Pets'
+import SignIn from './page/SignIn'
+import SignUp from './page/SignUp'
 
 function App() {
   return (
-        <Routes>
-          <Route path = "/" element={<HomePage/>}/>
-          <Route path = "/login" element={<SignIn/>}/>
-            <Route path = "/registry" element={<SignUp/>}/>
-        </Routes>
-      )
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/pets" element={<Pets />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/registry" element={<SignUp />} />
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/page/Pets.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const Pets = () => {
+  const pets = useSelector((state) => state.pets.pets)
+
+  return (
+    <div className="container">
+      <div className="row justify-content-between mt-5">
+        {pets.map((pet) => (
+          <div className="card col-3 m-2" key={pet._id}>
+            <img src={pet.img} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">{pet.header}</h5>
+              <p className="card-text">{pet.description}</p>
+              <span href="#" className="btn btn-primary w-100">
+                Перейти куда-нибудь
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Pets
