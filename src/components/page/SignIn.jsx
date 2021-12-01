bb7e3de048fe95818109103971d3ed21ce0064f6
Зелимхан Демильханов <zelemhanzzz@mail.ru>
2021-12-01 13:42:33 +0300

    функционал авторизации и регистрации

--- src/components/page/SignIn.jsx
+++ src/components/page/SignIn.jsx
@@ -1,11 +1,82 @@
-import React from 'react';
+import React, { useState } from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {auth} from "../../redux/features/signInReducer";
 
 const SignIn = () => {
-    return (
-        <div>
-              123
-        </div>
-    );
+  const [personalData, setPersonalData] = useState({});
+
+  const dispatch = useDispatch()
+
+    const error = useSelector(state => state.signIn.error)
+
+    const handlePersonalData = {
+        login: (login) => {
+            setPersonalData({...personalData, login})
+        },
+        password: (password) => {
+            setPersonalData({...personalData, password})
+        }
+    }
+
+  const handleSabmit = (personalDate) => {
+      dispatch(auth(personalData))
+  }
+
+  return (
+    <form>
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">
+          Email address
+        </label>
+        <input
+            onChange={(e)=>{
+                handlePersonalData.login(e.target.value)
+            }}
+          type="email"
+            value={personalData.login}
+          className="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Password
+        </label>
+        <input
+            onChange={(e)=>{
+                handlePersonalData.password(e.target.value)
+            }}
+            value={personalData.password}
+          type="password"
+          className="form-control"
+          id="exampleInputPassword1"
+        />
+          <div id="emailHelp" className="form-text">
+              {error}
+          </div>
+      </div>
+      <div className="mb-3 form-check">
+        <input
+          type="checkbox"
+          className="form-check-input"
+          id="exampleCheck1"
+        />
+        <label className="form-check-label" htmlFor="exampleCheck1">
+          Check me out
+        </label>
+      </div>
+      <button
+          onClick={()=>{
+              handleSabmit(personalData)
+          }}
+          type="submit"
+          className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default SignIn;
--- src/components/page/SignUp.jsx
+++ src/components/page/SignUp.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {createUser} from "../../redux/features/signUpReducer";
 
 const SignUp = () => {
+
+    const error = useSelector(state => state.signUp.error)
+
+    const [newUser, setNewUser] = useState({pets:[]});
+
+    const dispatch = useDispatch()
+
+    const handleNewUser = {
+        login: (login) => {
+            setNewUser({...newUser, login})
+        },
+        password: (password) => {
+            setNewUser({...newUser, password})
+        },
+        firstName: (firstName) => {
+            setNewUser({...newUser, firstName})
+        },
+        lastName: (lastName) => {
+            setNewUser({...newUser, lastName})
+        },
+        mail: (mail) => {
+            setNewUser({...newUser, mail})
+        },
+        phone: (phone) => {
+            setNewUser({...newUser, phone})
+        },
+    }
+
+    const handleSabmit = () => {
+        dispatch(createUser(newUser))
+    }
+//<input onChange={(e)=> handleNewUser.login(e.target.value)} value={newUser.login}/>
     return (
-        <div>
-            123
-        </div>
+
     );
 };
 
