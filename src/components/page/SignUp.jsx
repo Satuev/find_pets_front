1c338b3702d0a6dc5c98126afa0bf017cbae3c56
Atamazow <atamazov99@mail.ru>
2021-12-01 11:12:31 +0300

    Сделал инпуты для регистрации

--- src/components/page/SignUp.jsx
+++ src/components/page/SignUp.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from "react";
+import "./style.css"
 
 const SignUp = () => {
-    return (
+  return (
+    <div className='registar'>
+      <form className="form">
+        <h4>Зарегистрироваться</h4>
         <div>
-            123
+          <input
+            placeholder="Имя"
+            id="first_name"
+            type="text"
+            className="validate"
+          />
+          <input
+            id="last_name"
+            placeholder="Фамилия"
+            type="text"
+            className="validate"
+          />
+          <input
+            id="last_number"
+            placeholder="Номер телефона"
+            type="text"
+            className="validate"
+          />
         </div>
-    );
+        <div className="form-group">
+          <input className="form-input" placeholder="Логин" />
+          <input className="form-input" type="password" placeholder="Пароль" />
+          <input className="form-input" placeholder="email" />
+        </div>
+        <button type="button" className="form_button btn btn-primary">
+          войти
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
